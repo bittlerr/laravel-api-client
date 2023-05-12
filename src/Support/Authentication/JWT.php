bb6e7904f913d55729f23e20abe8b3e694712691
Constantin Cuciurcă <constantin@codegod.dev>
2023-05-12 16:01:21 +0300

    feat: upgrade jwt package

--- src/Support/Authentication/JWT.php
+++ src/Support/Authentication/JWT.php
@@ -2,17 +2,18 @@
 
 namespace MacsiDigital\API\Support\Authentication;
 
+use Firebase\JWT\Key;
 use Firebase\JWT\JWT as FirebaseJWT;
 
 class JWT
 {
-    public static function generateToken($token, $secret)
+    public static function generateToken($token, $secret, $alg = 'HS256')
     {
-        return FirebaseJWT::encode($token, $secret);
+        return FirebaseJWT::encode($token, $secret, $alg);
     }
 
-    public static function decodeToken($jwt, $secret)
+    public static function decodeToken($jwt, $secret, $alg = 'HS256')
     {
-        return FirebaseJWT::decode($jwt, $secret, ['HS256']);
+        return FirebaseJWT::decode($jwt, new Key($secret, $alg));
     }
 }
